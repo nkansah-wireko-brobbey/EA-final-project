2cf5339e7160d74886a1225d6f3a6b624ae6b5d4
Eyouel <eyouelaya@gmail.com>
2023-12-16 13:58:11 -0600

    Added RestAssusred Rest Test and ProductService class test.

---
+++ src/test/java/edu/miu/cs/cs544/controller/impl/ProductControllerTest.java
@@ -0,0 +1,36 @@
+package edu.miu.cs.cs544.controller.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductControllerTest {
+
+    @Test
+    void addProduct() {
+    }
+
+    @Test
+    void updateProduct() {
+    }
+
+    @Test
+    void deleteProduct() {
+    }
+
+    @Test
+    void getProduct() {
+    }
+
+    @Test
+    void getAllProducts() {
+    }
+
+    @Test
+    void handleRestErrors() {
+    }
+
+    @Test
+    void handleValidationExceptions() {
+    }
+}
--- src/test/java/edu/miu/cs/cs544/cs544/ProductRestTest.java
+++ src/test/java/edu/miu/cs/cs544/cs544/ProductRestTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.web.servlet.function.RequestPredicates.contentType;
 
 class ProductRestTest {
     @BeforeAll
