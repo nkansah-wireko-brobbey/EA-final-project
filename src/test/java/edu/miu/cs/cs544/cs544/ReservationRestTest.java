e533181295aa4696ce810367899d95cabf1fc311
aakarshan <aakartionsimkhada@gmail.com>
2023-12-18 16:52:34 -0600

    setup restTest for reservation

--- src/test/java/edu/miu/cs/cs544/cs544/ReservationRestTest.java
+++ src/test/java/edu/miu/cs/cs544/cs544/ReservationRestTest.java
@@ -1,4 +1,91 @@
 package edu.miu.cs.cs544.cs544;
 
+import edu.miu.cs.cs544.domain.ProductType;
+import edu.miu.cs.cs544.domain.ReservationType;
+import edu.miu.cs.cs544.domain.dto.ItemDTO;
+import edu.miu.cs.cs544.domain.dto.ProductDTO;
+import edu.miu.cs.cs544.domain.dto.ReservationDTO;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 public class ReservationRestTest {
+
+    @BeforeAll
+    public static void setUp() {
+        RestAssured.port = 8080;
+        RestAssured.baseURI = "http://localhost/api";
+    }
+
+
+    @Test
+    public void testCreateReservation() {
+        ReservationDTO reservationDTO = new ReservationDTO();
+        ItemDTO itemDTO = new ItemDTO();
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setName("Test Product");
+        productDTO.setExcerpt("Test Product");
+        productDTO.setDescription("This is a test product");
+        productDTO.setType(ProductType.Room);
+        productDTO.setMaxCapacity(2);
+        productDTO.setNightlyRate(100.0);
+        itemDTO.setProduct(productDTO);
+        itemDTO.setOccupants(5);
+        itemDTO.setCheckinDate(null);
+        itemDTO.setCheckoutDate(null);
+        itemDTO.setAuditData(null);
+        List<ItemDTO> itemDTOList = new ArrayList<>();
+        itemDTOList.add(itemDTO);
+        reservationDTO.setItems(itemDTOList);
+        reservationDTO.setReservationType(ReservationType.NEW);
+
+
+
+        Response response = given()
+                .body(reservationDTO)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/reservations")
+                .then()
+                .statusCode(201)
+                .extract()
+                .response();
+
+        int id = response.jsonPath().getInt("id");
+
+        System.out.println("Id: " + id + " Actual Response Body: " + response.asString());
+
+        given()
+                .when()
+                .get("/reservations/" + id)
+                .then()
+                .statusCode(200)
+                .body("items[0]", equalTo(itemDTO))
+                .body("auditData", equalTo(null))
+                .body("reservationType", equalTo(ReservationType.NEW));
+
+
+
+        given()
+                .when()
+                .delete("/reservations/" + id)
+                .then()
+                .statusCode(200);
+    }
+    @Test
+    public void testGetReservations(){
+
+    }
+    @Test
+    public void testGetReservationByID(){
+
+    }
 }
--- src/test/java/edu/miu/cs/cs544/service/impl/ReservationServiceImplementationTest.java
+++ src/test/java/edu/miu/cs/cs544/service/impl/ReservationServiceImplementationTest.java
@@ -1,14 +1,82 @@
 package edu.miu.cs.cs544.service.impl;
 
+import edu.miu.cs.cs544.domain.*;
+import edu.miu.cs.cs544.domain.adapter.CustomerAdapter;
+import edu.miu.cs.cs544.domain.adapter.ItemAdapter;
+import edu.miu.cs.cs544.domain.adapter.ProductAdapter;
+import edu.miu.cs.cs544.domain.adapter.ReservationAdapter;
+import edu.miu.cs.cs544.domain.dto.*;
+import edu.miu.cs.cs544.repository.CustomerRepository;
+import edu.miu.cs.cs544.repository.ProductRepository;
+import edu.miu.cs.cs544.repository.ReservationRepository;
+import edu.miu.cs.cs544.service.ProductService;
+import edu.miu.cs.cs544.service.ReservationService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
+@SpringBootTest
 class ReservationServiceImplementationTest {
 
-    @Test
-    void createReservation() {
+    @MockBean
+    private ReservationRepository reservationRepository;
+
+    @Autowired
+    private ReservationService reservationService;
+
+    @MockBean
+    private ProductRepository productRepository;
+
+    @Autowired
+    private ProductService productService;
+
+    @MockBean
+    private CustomerRepository customerRepository;
+
+
+    @TestConfiguration
+    static class ReservationServiceImplementationTestContextConfiguration {
+        @Bean
+        public ReservationServiceImplementation reservationServiceImplementation(CustomerRepository customerRepository,
+                                                                                 ReservationRepository reservationRepository,
+                                                                                 ProductRepository productRepository){
+            return new ReservationServiceImplementation(customerRepository,reservationRepository,productRepository);
+        }
     }
+
+
+    @Test
+    void createReservation() throws CustomError {
+        double nightlyRate = 100;
+        Product product = new Product(1, "Test Product", "Test Product", "This is a test product", ProductType.Room, nightlyRate, 2, true, null);
+        List<Item> itemList = new ArrayList<>();
+        Item item = new Item(1, 5, null, null, product, null);
+        itemList.add(item);
+        State stateDTO = new State(1, "22342", "Iowa", null);
+        Address addressDTO = new Address(1,"123 Ave", null, "Fairfield", stateDTO, "54333", null);
+        Customer customer = new Customer(1,"John","Doe", "john@gmail.com", null, addressDTO, addressDTO, null, null);
+//        customerDTO.setCustomerBillingAddressDTO(addressDTO);
+//        customerDTO.setCustomerBillingAddressDTO(addressDTO);
+//        ReservationDTO reservationDTO = new ReservationDTO(1, itemDTOList, null, ReservationType.NEW);
+//
+//        Mockito.when(productRepository.save(ProductAdapter.getProduct(productDTO))).thenReturn(ProductAdapter.getProduct(productDTO));
+////        Mockito.when(customerRepository.save(CustomerAdapter.getCustomer(customerDTO))).thenReturn(CustomerAdapter.getCustomer(customerDTO));
+
+        Reservation reservation = new Reservation(1,customer, itemList, null, ReservationType.NEW);
+        Mockito.when(reservationRepository.save(reservation)).thenReturn(reservation);
+        reservationService.createReservation(ReservationAdapter.getReservationDTO(reservation));
+        Mockito.verify(reservationRepository, Mockito.times(1)).save(reservation);
+
+    }
+
 
     @Test
     void getReservation() {
@@ -23,7 +91,21 @@ class ReservationServiceImplementationTest {
     }
 
     @Test
-    void deleteReservation() {
+    void deleteReservation() throws CustomError {
+        double nightlyRate = 100;
+        Product product = new Product(1, "Test Product", "Test Product", "This is a test product", ProductType.Room, nightlyRate, 2, true, null);
+        List<Item> itemList = new ArrayList<>();
+        Item item = new Item(1, 5, null, null, product, null);
+        itemList.add(item);
+        State stateDTO = new State(1, "22342", "Iowa", null);
+        Address addressDTO = new Address(1,"123 Ave", null, "Fairfield", stateDTO, "54333", null);
+        Customer customer = new Customer(1,"John","Doe", "john@gmail.com", null, addressDTO, addressDTO, null, null);
+        Reservation reservation = new Reservation(1,customer, itemList, null, ReservationType.NEW);
+        Optional<Reservation> optionalReservation = Optional.of(reservation);
+        Mockito.when(reservationRepository.findById(reservation.getId())).thenReturn(optionalReservation);
+        reservationService.deleteReservation(1);
+        Mockito.verify(reservationRepository, Mockito.times(1)).delete(optionalReservation.get());
+
     }
 
     @Test
