befd337d187e3bdf9caabbbf6dfacb0ff7ac434d
aakarshan <aakartionsimkhada@gmail.com>
2023-12-18 12:07:39 -0600

    Test files for Reservation Rest assured and Reservation Service is created

---
+++ src/test/java/edu/miu/cs/cs544/service/impl/ReservationServiceImplementationTest.java
@@ -0,0 +1,36 @@
+package edu.miu.cs.cs544.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReservationServiceImplementationTest {
+
+    @Test
+    void createReservation() {
+    }
+
+    @Test
+    void getReservation() {
+    }
+
+    @Test
+    void getAllReservation() {
+    }
+
+    @Test
+    void updateReservation() {
+    }
+
+    @Test
+    void deleteReservation() {
+    }
+
+    @Test
+    void getAllReservationByProductType() {
+    }
+
+    @Test
+    void getAllReservationByReservationType() {
+    }
+}
