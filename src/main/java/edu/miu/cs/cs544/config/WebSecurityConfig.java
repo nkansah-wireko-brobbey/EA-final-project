8a4c89085bb10ab72340a04a1187e7e282228412
aben-wg <aben.dev@gmail.com>
2023-12-20 01:44:07 -0600

    merge from master

--- src/main/java/edu/miu/cs/cs544/config/WebSecurityConfig.java
+++ src/main/java/edu/miu/cs/cs544/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2Clien
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,6 +21,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 @EnableConfigurationProperties(OAuth2ClientProperties.class)
+@Profile("prod")
 public class WebSecurityConfig {
 
     @Autowired
